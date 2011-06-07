69df17834b9eb0fd2d82376aa18df9bae3a03107
Patrick McCarty <patrick.mccarty@intel.com>
2011-06-07 16:11:13 -0700

    Add 'meego-ux-media' TS files to version control
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

---
+++ translations/meego-ux-media/meego-ux-media_en.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en" version="2.0">
+<context>
+    <name>PhotoDatabase</name>
+    <message>
+        <location filename="../../lib/photodatabase.cpp" line="142"/>
+        <source>Today</source>
+        <translation>Today</translation>
+    </message>
+    <message>
+        <location filename="../../lib/photodatabase.cpp" line="144"/>
+        <source>Yesterday</source>
+        <translation>Yesterday</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/meego-ux-media/meego-ux-media_lt.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt" version="2.0">
+<context>
+    <name>PhotoDatabase</name>
+    <message>
+        <location filename="../../lib/photodatabase.cpp" line="142"/>
+        <source>Today</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../lib/photodatabase.cpp" line="144"/>
+        <source>Yesterday</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
