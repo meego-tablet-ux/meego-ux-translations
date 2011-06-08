c7b931b32a7fde48ecda6e2da53cf0182fd94290
Patrick McCarty <patrick.mccarty@intel.com>
2011-06-08 14:02:14 -0700

    Pull in translations for 'meego-ux-sharing-qml-ui'
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

---
+++ translations/meego-ux-sharing-qml-ui/meego-ux-sharing-qml-ui_en_US.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en_US" version="2.0">
+<context>
+    <name>ShareButton</name>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareButton.qml" line="45"/>
+        <source>Share All</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareButton.qml" line="45"/>
+        <source>Share</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>ShareObj</name>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="100"/>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="142"/>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="474"/>
+        <source>Email</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="353"/>
+        <source>An error occurred when logging in to %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="361"/>
+        <source>Go to settings</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="396"/>
+        <source>Share by:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="466"/>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="576"/>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="611"/>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="642"/>
+        <source>Ok</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="506"/>
+        <source>Select service:</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="606"/>
+        <source>An error occurred while sharing: %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../../meego-ux-sharing-qml-ui/UI/ShareObj.qml" line="637"/>
+        <source>Upload progress: %1%%</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
